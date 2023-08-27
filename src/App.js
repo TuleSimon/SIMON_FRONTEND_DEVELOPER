5c4ebb2773bd1563f4ac273e23006848a404a78d
Tule Simon <58936865+TuleSimon@users.noreply.github.com>
2023-08-27 01:14:50 +0100

    editted README.MD

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
 import { useDispatch, useSelector } from "react-redux";
-import "./App.css";
 import GetStartedSection from "./components/GetStartedSection.tsx";
 import Navbar from "./components/Navbar";
 import {
